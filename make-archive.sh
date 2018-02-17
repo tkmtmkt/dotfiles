b218d16346d2be4d44e285c2841ae94af47e7854
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-02-17 19:32:36 +0900

    fix: リポジトリディレクトリのアーカイブからdeinのキャッシュフィアルを除外する

--- make-archive.sh
+++ make-archive.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
-BASE_DIR=$(dirname $SCRIPT_DIR)
 BASE_NAME=$(basename $SCRIPT_DIR)
+PARENT_DIR=$(dirname $SCRIPT_DIR)
 
-tar czf dotfiles-$(date +%Y%m%d).tar.gz -C $BASE_DIR \
+tar czf dotfiles-$(date +%Y%m%d).tar.gz -C $PARENT_DIR \
     --exclude=$BASE_NAME/dotfiles-*.tar.gz \
+    --exclude=$BASE_NAME/_vim/dein/cache_* \
+    --exclude=$BASE_NAME/_vim/dein/state_* \
     $BASE_NAME
