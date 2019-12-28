16d07e2842cc0d8deca892e71f38ee92106cdc1b
Takamatsu Makoto <tkmtmkt@gmail.com>
2019-12-28 23:40:02 +0900

    modify: Change base directory of setup script

--- set_env.sh
+++ set_env.sh
@@ -4,21 +4,22 @@
 # 使用方法
 # $ . ~/.dotfiles/set_env.sh
 #
+SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
 
 # .bash_profileに_bash_profile読み込み処理を追記する
-TMP="~/.dotfiles/_bash_profile"
+TMP="${SCRIPT_DIR}/_bash_profile"
 [[ -z $(grep -o "$TMP" ~/.bash_profile) ]] &&
   echo "[[ -s $TMP ]] && . $TMP" >> ~/.bash_profile
 
 # .bashrcに_bashrc読み込み処理を追記する
-TMP="~/.dotfiles/_bashrc"
+TMP="${SCRIPT_DIR}/_bashrc"
 [[ -z $(grep -o "$TMP" ~/.bashrc) ]] &&
   echo "[[ -s $TMP ]] && . $TMP" >> ~/.bashrc
 
 # シンボリックリンクを作成する
 while read NAME
 do
-  SOURCE=".dotfiles/_${NAME}"
+  SOURCE="${SCRIPT_DIR}/_${NAME}"
   TARGET="${HOME}/.${NAME}"
   [[ -L $TARGET ]] && rm $TARGET
   [[ ! -e $TARGET ]] && ln -sf $SOURCE $TARGET
